fb63fb3b0c058fc0b0ed918ea2edf6dc37935ca3
Yussuf Shariff <yussuf.shariff@booking.com>
2025-12-03 11:34:16 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Todo index page</h1>"
+
+@app.route('/tasks')
+def all_tasks():
+    return "<h1> List of all tasks </h1>"
+
+@app.route('/task/<int:task_id>')
+def task(task_id):
+    return f"<h1>Task detail page for task {task_id}</h1>"
+
+@app.route('/new-task')
+def new_task():
+    return "<h1>Enter your new task</h1>"
+
