a26b5daa5e605c9e431d6cfed74f66fc8b749168
Yussuf Shariff <yussuf.shariff@booking.com>
2025-12-03 12:34:20 +0000

    Passing dynamic todo_count length to home page

--- routes.py
+++ routes.py
@@ -1,9 +1,31 @@
 from app import app
 from flask import render_template
 
+todos = [
+    {
+        "id": 1,
+        "title": "title1",
+        "description": "description1",
+        "created_at": "2025-11-12 06:27:37",
+    },
+    {
+        "id": 2,
+        "title": "title2",
+        "description": "description2",
+        "created_at": "2025-11-12 06:27:37",
+    },
+    {
+        "id": 3,
+        "title": "title3",
+        "description": "description3",
+        "created_at": "2025-11-12 06:27:37",
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    todo_count = len(todos)
+    return render_template('index.html', todo_count=todo_count)
 
 @app.route('/tasks')
 def all_tasks():
